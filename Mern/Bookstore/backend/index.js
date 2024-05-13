849276556b3ebbed324931552b2e01e0f3aab2d5
salahvk <salahkoyilandy@gmail.com>
2024-05-13 23:23:14 +0530

    get method implemented for book data getting

--- Mern/Bookstore/backend/index.js
+++ Mern/Bookstore/backend/index.js
@@ -1,16 +1,54 @@
 import express from "express";
-import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { PORT, mongoDBURL } from "./config.js";
+import { Book } from "./models/bookmodel.js";
 
 const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
 
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Welcome Salah");
 });
 
+app.get("/books", async (request, response) => {
+  try {
+    const books = await Book.find({});
+    return response.status(200).json(books);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Send all required fields: title, author, publishYear",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
 mongoose
-  .connect()
+  .connect(mongoDBURL)
   .then(() => {
     console.log("App connected to DB");
     app.listen(PORT, () => {
