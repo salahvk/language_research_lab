a6a8040c536512b2f372334b3bba72e1f00c2d70
salahvk <salahkoyilandy@gmail.com>
2024-05-14 09:02:03 +0530

    update a book and delete a book features added

--- Mern/Bookstore/backend/index.js
+++ Mern/Bookstore/backend/index.js
@@ -13,16 +13,35 @@ app.get("/", (request, response) => {
   return response.status(234).send("Welcome Salah");
 });
 
+// get all books
 app.get("/books", async (request, response) => {
   try {
     const books = await Book.find({});
-    return response.status(200).json(books);
+    return response.status(200).json({
+      count: books.length,
+      data: books,
+    });
   } catch (error) {
     console.log(error.message);
     response.status(500).send({ message: error.message });
   }
 });
 
+// get book by id
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const book = await Book.findById(id);
+
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// create a book
 app.post("/books", async (request, response) => {
   try {
     if (
@@ -43,6 +62,44 @@ app.post("/books", async (request, response) => {
     return response.status(201).send(book);
   } catch (error) {
     console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// update a book
+
+app.put("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+    return response.status(200).send({ message: "Book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+// delete a book
+
+app.delete("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+
+    const result = await Book.findByIdAndDelete(id);
+
+    if (!result) {
+      return response.status(404).json({ message: "Book not found" });
+    }
+
+    return response.status(200).send({ message: "Book deleted successfully" });
+    
+  } catch (error) {
+    console.log(error);
     response.status(500).send({ message: error.message });
   }
 });
