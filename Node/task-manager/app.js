c8ed765d712b686111cf5ffcbc8e1063a0040528
salahvk <salahkoyilandy@gmail.com>
2024-08-20 19:52:47 +0530

    tasks controllers and routes created

--- Node/task-manager/app.js
+++ Node/task-manager/app.js
@@ -1,1 +1,19 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json());
+
+app.get('/hello',(req,res)=>{
+    res.send('Hi salah');
+})
+
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000
+
+app.listen(port,console.log(`Server is listening on port ${port}..`))
+
+
+
---
+++ Node/task-manager/controllers/tasks.js
@@ -0,0 +1,21 @@
+const getAllTasks = (req, res) => {
+  res.send("all tasks");
+};
+
+const getTask = (req, res) => {
+  res.send("get single task");
+};
+
+const createTask = (req, res) => {
+  res.send("create task");
+};
+
+const updateTask = (req, res) => {
+  res.send("update task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("delete task");
+};
+
+module.exports = { getAllTasks };
