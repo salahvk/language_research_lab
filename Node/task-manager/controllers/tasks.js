b7befd697785a701a2260df25eff87f89bce8cb4
salahvk <salahkoyilandy@gmail.com>
2024-08-21 23:40:57 +0530

    getalltasks and  create task api's added

--- Node/task-manager/controllers/tasks.js
+++ Node/task-manager/controllers/tasks.js
@@ -1,13 +1,25 @@
-const getAllTasks = (req, res) => {
-  res.send("all tasks");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const getTask = (req, res) => {
-  res.json({id:req.params.id});
+  res.json({ id: req.params.id });
 };
 
-const createTask = (req, res) => {
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
