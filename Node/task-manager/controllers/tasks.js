af7df0dd4c5d785a394db808aaaa3ae02958f7b2
salahvk <salahkoyilandy@gmail.com>
2024-08-23 08:03:11 +0530

    get task api added

--- Node/task-manager/controllers/tasks.js
+++ Node/task-manager/controllers/tasks.js
@@ -9,8 +9,17 @@ const getAllTasks = async (req, res) => {
   }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask =async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task =await Task.findOne({ _id: taskID });
+    if(!task){
+      return res.status(404).json({msg:`No task with id : ${taskID}`})
+    }
+    res.status(200).json({task})
+  } catch (error) {
+    res.status(500).json({error})
+  }
 };
 
 const createTask = async (req, res) => {
